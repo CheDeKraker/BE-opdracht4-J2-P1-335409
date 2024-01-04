4e2dfc99600d7b915293fc016ea0e75105920cbd
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-01-04 23:09:38 +0100

    config goed aangepast

--- app/config/config.php
+++ app/config/config.php
@@ -3,5 +3,5 @@
 define('DB_HOST',  'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'BE_opdracht1_J2_P1');
-define('URLROOT', 'http://www.php-opdracht2.com');
+define('DB_NAME', 'Be-opdracht-02');
+define('URLROOT', 'http://www.php-opdracht4.com');
